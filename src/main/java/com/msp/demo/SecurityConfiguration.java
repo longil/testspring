46cbe41ba95cafc1595aaed88e8a07eb8cc1ef9e
Châu Long <chaulongdbp@gmail.com>
2020-02-25 03:42:52 +0700

    MySql Jpa

--- src/main/java/com/msp/demo/SecurityConfiguration.java
+++ src/main/java/com/msp/demo/SecurityConfiguration.java
@@ -2,13 +2,13 @@ package com.msp.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
@@ -19,42 +19,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public DataSource dataSource;
 
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.h2.Driver");
-//        dataSource.setUsername("sa");
-//        dataSource.setPassword("");
-//        dataSource.setUrl("jdbc:h2:~/test");
-//        return dataSource;
-//    }
+    @Autowired
+    private UserDetailsService userDetailsServiceImpl;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password(encoder().encode("user"))
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password(encoder().encode("admin"))
-//                .roles("ADMIN");
-
-        auth.jdbcAuthentication()
-                .dataSource(dataSource); //KO define datasource nên dùng H2 db làm default
-
-//                .withDefaultSchema() // sử dụng schema default, nếu tạo schema thật thì comment dòng này
-
-                /*Dùng theo schema default của spring*/
-//        .withUser(
-//                User.withUsername("user").password(encoder().encode("user")).roles("USER")
-//        ).withUser(
-//                User.withUsername("admin").password(encoder().encode("admin")).roles("ADMIN")
-//        );
-
-                /*Dùng theo custom schema*/
-//                .usersByUsernameQuery("")
-//                .authoritiesByUsernameQuery("");
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(encoder());
     }
 
     @Override
@@ -69,6 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
